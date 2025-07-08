7ce592ef2d2234e5c360efab107d4d64e47cb8f1
Guilherme Recalcatte Vogel <guilhermerecalcatte@gmail.com>
2025-07-08 16:06:29 -0300

    melhorado graficos

--- App/Model/Entity/Notas.php
+++ App/Model/Entity/Notas.php
@@ -69,7 +69,7 @@ class Notas
     public static function getNotasByEquipe($equipe)
     {
         if ($equipe == 'todas') {
-            return self::getNotas();
+            return self::getNotas('1=1');
 
         }
         return self::getNotas('equipe = "' . $equipe . '"');
