eb6b8b6000beb95d57c836a81a1e1820d748bbae
Guilherme Recalcatte Vogel <guilhermerecalcatte@gmail.com>
2025-07-10 09:43:00 -0300

    e-mails em lotes paralelos 100

--- cronjobs/filaEmails.php
+++ cronjobs/filaEmails.php
@@ -1,54 +1,53 @@
 <?php
-
-namespace App\Cronjobs;
-require __DIR__ . '/../includes/app.php';
-
-use \App\Model\Entity\FilaEmails as EntityFilaEmails;
-use DateTime;
-use DateTimeZone;
-
-$limite = 300;
-$results = EntityFilaEmails::getFilaEmails('status = "pendente"', 'id ASC', $limite);
-
+$maxParallel = 10; // limite de processos ativos
 $processes = [];
 $pipesList = [];
+$queue = [];
 
-// Dispara subprocessos
+// monta a fila
 while ($obFilaEmails = $results->fetchObject(EntityFilaEmails::class)) {
-    $cmd = 'php ' . escapeshellarg(__DIR__ . '/send_email.php') . ' ' .
-        escapeshellarg($obFilaEmails->id);
-
-    // Abre o processo e captura stdout
-    $descriptorspec = [
-        1 => ['pipe', 'w'], // stdout
-        2 => ['pipe', 'w']  // stderr (opcional)
-    ];
-
-    $process = proc_open($cmd, $descriptorspec, $pipes);
-
-    if (is_resource($process)) {
-        $processes[] = $process;
-        $pipesList[] = $pipes;
-    }
+    $queue[] = $obFilaEmails->id;
 }
 
-// Ler saídas
-foreach ($processes as $index => $process) {
-    $stdout = stream_get_contents($pipesList[$index][1]);
-    fclose($pipesList[$index][1]);
+while (!empty($queue) || !empty($processes)) {
+    // inicia novos processos até atingir o limite
+    while (count($processes) < $maxParallel && !empty($queue)) {
+        $id = array_shift($queue);
+        $cmd = 'php ' . escapeshellarg(__DIR__ . '/send_email.php') . ' ' . escapeshellarg($id);
+        $descriptorspec = [
+            1 => ['pipe', 'w'],
+            2 => ['pipe', 'w']
+        ];
+        $process = proc_open($cmd, $descriptorspec, $pipes);
 
-    $stderr = stream_get_contents($pipesList[$index][2]);
-    fclose($pipesList[$index][2]);
-
-    proc_close($process);
-
-    // Exibir o que cada subprocesso imprimiu
-    echo $stdout;
-    if (!empty($stderr)) {
-        echo "Erro: " . $stderr;
+        if (is_resource($process)) {
+            $processes[] = $process;
+            $pipesList[] = $pipes;
+        }
     }
-}
 
-// Registrar conclusão do lote
-$data = new DateTime('now', new DateTimeZone('America/Sao_Paulo'));
-echo "Lote concluído em paralelo - " . $data->format('d/m/Y H:i') . "\n";
+    // verifica processos concluídos
+    foreach ($processes as $index => $process) {
+        $status = proc_get_status($process);
+        if (!$status['running']) {
+            $stdout = stream_get_contents($pipesList[$index][1]);
+            fclose($pipesList[$index][1]);
+
+            $stderr = stream_get_contents($pipesList[$index][2]);
+            fclose($pipesList[$index][2]);
+
+            proc_close($process);
+
+            echo $stdout;
+            if (!empty($stderr)) {
+                echo "Erro: " . $stderr;
+            }
+
+            unset($processes[$index]);
+            unset($pipesList[$index]);
+        }
+    }
+
+    // evita loop apertado
+    usleep(100000); // 0.1 segundo
+}
